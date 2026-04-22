d501630369068175cba2af163ec4aa4d193e284c
Vinícius F. <fonsecavinicius117@gmail.com>
2026-04-22 20:49:07 -0300

    feat: implementação de getters e setters na classe Aluno

--- src/Aluno.java
+++ src/Aluno.java
@@ -2,9 +2,41 @@ public class Aluno{
 
     //Atributos privados
     private String nome;
-    private long matricula;
+    private int matricula;
     private int idade;
     private double notaFinal;
 
+    // GETTERS E SETTERS
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getNotaFinal() {
+        return notaFinal;
+    }
+
+    public void setNotaFinal(double notaFinal) {
+        this.notaFinal = notaFinal;
+    }
 }
