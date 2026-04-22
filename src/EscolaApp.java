d8f5ae141ad69c81a35203460e3954470224bd90
Vinícius F. <fonsecavinicius117@gmail.com>
2026-04-22 20:52:59 -0300

    feat: implementação de getters e setters na classe EscolaApp

--- src/EscolaApp.java
+++ src/EscolaApp.java
@@ -1,7 +1,34 @@
 public class EscolaApp {
 
     // Atributos privados
+
     private String nomeEscola;
     private String cidade;
     private int quantidadeSalas;
+
+    // GETTERS E SETTERS
+
+    public String getNomeEscola() {
+        return nomeEscola;
+    }
+
+    public void setNomeEscola(String nomeEscola) {
+        this.nomeEscola = nomeEscola;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public int getQuantidadeSalas() {
+        return quantidadeSalas;
+    }
+
+    public void setQuantidadeSalas(int quantidadeSalas) {
+        this.quantidadeSalas = quantidadeSalas;
+    }
 }
